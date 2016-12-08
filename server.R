0b6bdac5a19c53718c6fbad8164802d5c467a388
scottdalwood <scottdalwood@gmail.com>
2016-12-08 17:38:20 +1100

    Add 'leg' to data and enable subsetting with selectInput

--- server.R
+++ server.R
@@ -4,17 +4,31 @@ shinyServer(function(input, output) {
 ####################################################################################################
 
 # Data ####
+map.data <- reactive({
   
+  # subset data based on user selection
+  if (input$selLeg == "All"){
+    df <- shows
+  } else {
+    df <- subset(shows, shows$Leg == input$selLeg)
+  }
+  
+  # print object 
+  df
+  
+})
+
+#__________________________________________________________________________________________________# 
   
 # Map ####
 output$map <- renderLeaflet({
   
   leaflet() %>% 
     addTiles() %>% 
-    addMarkers(lat     = shows$Latitude
-               , lng   = shows$Longitude
-               , popup = paste("<b>City:</b>", shows$City, "<br>"
-                               , "<b>Venue:</b>", shows$Venue))
+    addMarkers(lat     = map.data()$Latitude
+               , lng   = map.data()$Longitude
+               , popup = paste("<b>City:</b>", map.data()$City, "<br>"
+                               , "<b>Venue:</b>", map.data()$Venue))
   
 })
 
--- ui.R
+++ ui.R
@@ -6,17 +6,21 @@ shinyUI(
     
     h2(titlePanel("Bruce Springsteen and the E Street Band")),
     h3(titlePanel("The River 2016 & Summer '17 Tours")), 
-    
+    br(),
     sidebarLayout(
       # sidebar ui input
       sidebarPanel(
+        selectInput(inputId = "selLeg"
+                    , label = "Select leg of tour:"
+                    , choices = c("All","North America 1","Europe","North America 2","Australia/NZ")
+                    , selected = "All"
+        ), 
         width = 3
-        
       ),
         
-      # data table
+      # map output
       mainPanel( 
-        leafletOutput('map', height = 600)
+        leafletOutput('map', height = 450)
       ) 
     )
   ) 
